1d56a7861fbc074c8db66dccf780e67d165c6aab
erdes10032 <erdes10032@gmail.com>
2025-10-24 13:53:26 +0300

    2

---
+++ NewsPaper/news/templatetags/custom_filters.py
@@ -0,0 +1,28 @@
+from django import template
+from django.core.exceptions import BadRequest
+
+register = template.Library()
+
+@register.filter()
+def censor(value):
+   if not isinstance(value, str):
+      raise TypeError('Value must be a string')
+
+   BAD_WORDS = ['fack', 'shit', 'bitch']
+
+   censored_text = value
+   for word in BAD_WORDS:
+      words = censored_text.split()
+      censored_words = []
+      for w in words:
+         if word in w.lower():
+            censored_words.append(w[0] + '*' * (len(w) - 1))
+         else:
+            censored_words.append(w)
+      censored_text = ' '.join(censored_words)
+
+   return censored_text
+
+@register.filter
+def join_categories(value):
+    return ", ".join([category.name for category in value])
