7ade78f04e9ae0c29a9c98085e4ff0b085e6771b
erdes10032 <erdes10032@gmail.com>
2025-11-21 16:54:55 +0300

    7

--- NewsPaper/NewsPaper/settings.py
+++ NewsPaper/NewsPaper/settings.py
@@ -251,6 +251,7 @@ LOGGING = {
             'class': 'logging.FileHandler',
             'filename': 'general.log',
             'formatter': 'general',
+            'filters': ['require_debug_false']
         },
         'errors': {
             'level': 'ERROR',
